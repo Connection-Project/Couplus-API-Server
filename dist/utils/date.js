237df6c5ab2d2b7d9064530cf65bd6747c35c8ae
Ansu-dev <ansu1007@noutecompany.com>
2023-02-06 14:32:13 +0900

    Fix(utils):formatDateParam 초 필드 제외

--- dist/utils/date.js
+++ dist/utils/date.js
@@ -18,8 +18,7 @@ const formatDateParam = (date) => {
     const day = ('0' + date.getDate()).slice(-2);
     const hours = ('0' + date.getHours()).slice(-2);
     const minutes = ('0' + date.getMinutes()).slice(-2);
-    const seconds = ('0' + date.getSeconds()).slice(-2);
-    return year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
+    return year + '-' + month + '-' + day + ' ' + hours + ':' + minutes;
 };
 exports.formatDateParam = formatDateParam;
 const currentDateFormat = () => {
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -15,8 +15,7 @@ export const formatDateParam = (date: Date) => {
     const day = ('0' + date.getDate()).slice(-2);
     const hours = ('0' + date.getHours()).slice(-2);
     const minutes = ('0' + date.getMinutes()).slice(-2);
-    const seconds = ('0' + date.getSeconds()).slice(-2);
-    return year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
+    return year + '-' + month + '-' + day + ' ' + hours + ':' + minutes;
 };
 
 export const currentDateFormat = () => {
